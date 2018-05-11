82a21a4ccd75f3d88dd161710497f717e7e8aa22
Leslie Stevens <flylady@mail.utexas.edu>
2018-05-11 22:36:27 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season}:".capitalize
     holidays.each do |holiday, supplies|
       holiday.to_s
-      holiday.split
+      "holiday".split
       holiday.each do |item|
         item.capitalize
         holiday.join
