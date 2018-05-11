3a52b3ffc624d2ceeb04abd68372c17e03395940
Leslie Stevens <flylady@mail.utexas.edu>
2018-05-11 22:45:11 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
       "#{holiday}".split
       holiday.each {|item|
         item.capitalize}
-      end
+
       holiday.join
       puts "  " + "#{holiday}".capitalize + ": " + supplies.join(", ")
 
