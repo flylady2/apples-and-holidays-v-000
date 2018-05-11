30927da422acef34e8cf28cc2e01549d87f825c1
Leslie Stevens <flylady@mail.utexas.edu>
2018-05-11 21:43:57 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,6 +51,16 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
+  array = []
+  holiday_hash.each do |season, holidays|
+    holidays.each do |holiday, supplies|
+      if season == :winter
+        array.push(supplies)
+      end
+    end
+  end
+  array
+
 
 
 
