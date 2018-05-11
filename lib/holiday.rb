c5d72f1e48ac8693fedf7c0cf4fb7031013b1106
Leslie Stevens <flylady@mail.utexas.edu>
2018-05-11 21:57:30 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,15 +58,7 @@ def all_winter_holiday_supplies(holiday_hash)
     end  
   end
   array
-
-
-
-
-
-
-
   # return an array of all of the supplies that are used in the winter season
-
 end
 
 def all_supplies_in_holidays(holiday_hash)
